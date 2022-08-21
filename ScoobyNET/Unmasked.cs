94fd47d37f3495d6e38fbbbc2d2b1c497b2ac4f1
HDBSD <6626255+HDBSD@users.noreply.github.com>
2022-08-21 14:42:44 +0100

    Built in byte swapping now used, and added additional addresses

--- ScoobyNET/Unmasked.cs
+++ ScoobyNET/Unmasked.cs
@@ -12,16 +12,40 @@ namespace ScoobyNET
         public static uint getHealth()
         {
             byte[] buff = new byte[4];
-            DolphinAccessor.readFromRAM(0x559900, ref buff, 4, false);
-            Array.Reverse(buff);
+            DolphinAccessor.readFromRAM(0x559900, ref buff, 4, true);
             return BitConverter.ToUInt32(buff, 0);
         }
+
+        //does not exist
+        /*
         public static uint getTotalHealth()
         {
             byte[] buff = new byte[4];
-            DolphinAccessor.readFromRAM(0x5598FC, ref buff, 4, false);
-            Array.Reverse(buff);
+            DolphinAccessor.readFromRAM(0x5598FC, ref buff, 4, true);
             return BitConverter.ToUInt32(buff, 0);
         }
+        */
+
+        public static uint getMubber()
+        {
+            byte[] buff = new byte[4];
+            DolphinAccessor.readFromRAM(0x559908, ref buff, 4, true);
+            return BitConverter.ToUInt32(buff, 0);
+        }
+
+        public static uint getLevel()
+        {
+            byte[] buff = new byte[4];
+            DolphinAccessor.readFromRAM(0x5599F1, ref buff, 4, true);
+            return BitConverter.ToUInt32(buff, 0);
+        }
+
+       public static uint getFoodID()
+        {
+            byte[] buff = new byte[4];
+            DolphinAccessor.readFromRAM(0x55999C, ref buff, 4, true);
+            return BitConverter.ToUInt32(buff, 0);
+        }
+
     }
 }
