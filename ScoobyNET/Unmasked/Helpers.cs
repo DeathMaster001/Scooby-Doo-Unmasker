495ae4a7c9e64b75007bfb730590677e5eb36065
HDBSD <6626255+HDBSD@users.noreply.github.com>
2022-08-21 21:56:43 +0100

    ported over a large chunk of the lua scripts

---
+++ ScoobyNET/Unmasked/Helpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoobyNET.Unmasked
+{
+    public class Helpers
+    {
+        public static void reverseBits(BitArray array)
+        {
+            int length = array.Length;
+            int mid = (length / 2);
+
+            for (int i = 0; i < mid; i++)
+            {
+                bool bit = array[i];
+                array[i] = array[length - i - 1];
+                array[length - i - 1] = bit;
+            }
+        }
+    }
+}
---
+++ ScoobyNET/Unmasked/Level.cs
@@ -0,0 +1,46 @@
+﻿//based on https://github.com/DeathMaster001/Scooby-Doo-Unmasked-Lua-Tool/blob/main/sdu/level.lua
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoobyNET.Unmasked
+{
+    public class Level
+    {
+        private static Dictionary<uint, string> levelLookup= new Dictionary<uint, string>
+		{
+			[2] = "Monster Profiles",
+			[3] = "Bonus Art",
+			[4] = "Main Menu",
+			[6] = "MFM 1",
+			[7] = "Chinatown Hub",
+			[8] = "Cookie Factory",
+			[9] = "Sewers",
+			[10] = "Sewers Autoscroll/End",
+			[11] = "Temple",
+			[12] = "Warehouse - Dragon",
+			[13] = "Theme Park Hub",
+			[14] = "Haunted House",
+			[15] = "Water Park",
+			[16] = "Water Slide/End",
+			[17] = "Circus",
+			[18] = "House of Mirrors - Guitar Ghoul",
+			[19] = "Museum Hub",
+			[20] = "Dino",
+			[21] = "Medieval",
+			[22] = "Undersea",
+			[23] = "Planetarium - Caveman",
+			[25] = "MFM2",
+			[26] = "MFM3 - Pterodactyl",
+			[27] = "MFM2 Mystery Machine"
+		};
+
+		public static string getLevelName()
+        {
+			return levelLookup[Memory.getLevel()];
+        }
+    }
+}
