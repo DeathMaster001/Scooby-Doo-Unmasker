2b43ccfc0ac9526d5dccfd810ad6d83913c213a5
DeathMaster001 <austinross173@gmail.com>
2022-09-12 02:45:26 -0400

    Collectibles: Removed getFoodTotals, updated comments, implemented clues and traps.
    Memory: Removed getFoodID, implemented getPosition

--- ScoobyNET/Unmasked/Collectibles.cs
+++ ScoobyNET/Unmasked/Collectibles.cs
@@ -60,11 +60,6 @@ namespace ScoobyNET.Unmasked
 			[22] = new string[] { "?", "?", "Cotton Candy", "Cabbage", "Shrimp", "Broccoli", "Hot Pepper", "Eggplant" },	//"Undersea",
 		};
 
-		public static string getFoodTotals()
-		{
-			return "TODO: Collectibles.lua: getFoodTotals\n";
-		}
-
 		public static Dictionary<string, bool>getLevelFoods()
         {
 			Dictionary<string, bool> levelFoods = new Dictionary<string, bool>();
@@ -89,10 +84,171 @@ namespace ScoobyNET.Unmasked
                 }
             }
 
-
 			return levelFoods;
 
 		}
 
-    }
+		private static Dictionary<uint, List<uint>> levelClueMap = new Dictionary<uint, List<uint>>
+		{
+			[2] = null,										//"Monster Profiles",
+			[3] = null,										//"Bonus Art",
+			[4] = null,										//"Main Menu",
+			[6] = new List<uint> { 0x55999C },				//"MFM 1",
+			[7] = new List<uint> { 0x55999C },				//"Chinatown Hub",
+			[8] = new List<uint> { 0x55999C },				//"Cookie Factory",
+			[9] = new List<uint> { 0x55999C },				//"Sewers",
+			[10] = new List<uint> { 0x55999C },				//"Sewers Autoscroll/End",
+			[11] = new List<uint> { 0x55999D },				//"Temple",
+			[12] = null,									//"Warehouse/\nDragon",
+			[13] = new List<uint> { 0x55999D },				//"Theme Park Hub",
+			[14] = new List<uint> { 0x55999D },				//"Haunted House",
+			[15] = new List<uint> { 0x55999D, 0x55999E },   //"Water Park",
+			[16] = null,									//"Water Slide/End",
+			[17] = new List<uint> { 0x55999E },				//"Circus",
+			[18] = null,									//"House of Mirrors/\nGuitar Ghoul",
+			[19] = new List<uint> { 0x55999E },				//"Museum Hub",
+			[20] = new List<uint> { 0x55999F },				//"Dino",
+			[21] = new List<uint> { 0x5599AF, 0x5599A0 },	//"Medieval",
+			[22] = new List<uint> { 0x5599A0 },				//"Undersea",
+			[23] = null,									//"Planetarium/\nCaveman",
+			[25] = null,									//"MFM2",
+			[26] = null,									//"MFM3/\nPterodactyl",
+			[27] = null,									//"MFM2 Mystery Machine",
+		};
+
+		private static Dictionary<uint, string[]> clueBitMap = new Dictionary<uint, string[]>
+		{
+            [6] = new string[] { "?", "?", "?", "?", "?", "?", "Beacon", "Keycard" },														//"MFM 1",
+			[7] = new string[] { "?", "?", "?", "?", "?", "Cookie", "?", "?" },																//"Chinatown Hub",
+			[8] = new string[] { "?", "?", "?", "Scale", "Key", "?", "?", "?" },															//"Cookie Factory",
+			[9] = new string[] { "Stone", "Weight", "?", "?", "?", "?", "?", "?" },                                                         //"Sewers",
+			[10] = new string[] { "?", "?", "Bell", "?", "?", "?", "?", "?" },                                                              //"Sewers Autoscroll/End",
+			[11] = new string[] { "?", "?", "?", "?", "?", "Lever", "Menu", "UV slip" },                                                    //"Temple",
+			[13] = new string[] { "?", "?", "?", "?", "Ticket", "?", "?", "?" },                                                            //"Theme Park Hub",
+			[14] = new string[] { "?", "Photo", "Spring", "Tape", "?", "?", "?", "?" },                                                     //"Haunted House",
+			[15] = new string[] { "Album", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "Key", "Knob", "Tomato" },           //"Water Park",
+			[17] = new string[] { "?", "Brochure", "Hammer", "Tennis Ball", "Music", "?", "?", "?" },                                       //"Circus",
+			[19] = new string[] { "Photo", "?", "?", "?", "?", "?", "?", "?" },                                                             //"Museum Hub",
+			[20] = new string[] { "?", "?", "?", "?", "Key", "Chain", "Saturn", "Bone" },                                                   //"Dino",
+			[21] = new string[] { "Contract", "Shield", "Fuse", "Sword", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "Armor" },  //"Medieval",
+			[22] = new string[] { "?", "Glasses", "Tripod", "Guide", "Building", "Light", "Video", "?" },                                   //"Undersea",
+		};
+
+		public static Dictionary<string, bool> getLevelClues()
+		{
+			Dictionary<string, bool> levelClues = new Dictionary<string, bool>();
+
+			uint lvl = Memory.getLevel();
+			List<uint> lvlFlagAddr = levelClueMap[lvl];
+
+			if (lvlFlagAddr.Any())
+			{
+				BitArray clueFlags;
+
+				if (lvlFlagAddr.Count == 1)
+				{
+					byte[] buff = new byte[1];
+                    DolphinAccessor.readFromRAM(lvlFlagAddr[0], ref buff, 1, false);
+					clueFlags = new BitArray(buff);
+					Helpers.reverseBits(clueFlags);
+				}
+				else
+                {
+					byte[] buff = new byte[1];
+					byte[] buff2 = new byte[2];
+					DolphinAccessor.readFromRAM(lvlFlagAddr[0], ref buff, 1, false);
+					
+					buff2[0] = buff[0];
+					buff = new byte[1];
+					DolphinAccessor.readFromRAM(lvlFlagAddr[1], ref buff, 1, false);
+
+					buff2[1] = buff[0];
+
+					clueFlags = new BitArray(buff2);
+					Helpers.reverseBits(clueFlags);
+				}
+
+				for (int i = 0; i <= clueFlags.Length - 1; i++)
+				{
+					if (clueBitMap[lvl][i] != "?")
+					{
+						levelClues.Add(clueBitMap[lvl][i], clueFlags[i]);
+					}
+				}
+			}
+
+			return levelClues;
+
+		}
+
+		private static Dictionary<uint, uint?> levelTrapMap = new Dictionary<uint, uint?>
+		{
+			[2] = null,         //"Monster Profiles",
+			[3] = null,         //"Bonus Art",
+			[4] = null,         //"Main Menu",
+			[6] = null,			//"MFM 1",
+			[7] = null,			//"Chinatown Hub",
+			[8] = 0x5599AF,     //"Cookie Factory",
+			[9] = 0x5599AF,     //"Sewers",
+			[10] = null,		//"Sewers Autoscroll/End",
+			[11] = 0x5599AF,    //"Temple",
+			[12] = null,        //"Warehouse/\nDragon",
+			[13] = null,		//"Theme Park Hub",
+			[14] = 0x5599AF,    //"Haunted House",
+			[15] = 0x5599AF,	//"Water Park",
+			[16] = null,		//"Water Slide/End",
+			[17] = 0x5599AF,    //"Circus",
+			[18] = null,        //"House of Mirrors/\nGuitar Ghoul",
+			[19] = null,		//"Museum Hub",
+			[20] = 0x5599AF,	//"Dino",
+			[21] = 0x5599AF,	//"Medieval",
+			[22] = 0x5599AE,    //"Undersea",
+			[23] = null,        //"Planetarium/\nCaveman",
+			[25] = null,        //"MFM2",
+			[26] = null,        //"MFM3/\nPterodactyl",
+			[27] = null,        //"MFM2 Mystery Machine",
+		};
+
+		private static Dictionary<uint, string[]> trapBitMap = new Dictionary<uint, string[]>
+		{
+            [8] = new string[] { "?", "?", "?", "?", "?", "?", "?", "Broom" },			//"Cookie Factory",
+			[9] = new string[] { "?", "?", "?", "?", "?", "?", "Gas Can", "?" },		//"Sewers",
+			[11] = new string[] { "?", "?", "?", "?", "?", "Rope", "?", "?" },			//"Temple",
+			[14] = new string[] { "?", "?", "?", "?", "Lantern", "?", "?", "?" },	    //"Haunted House",
+			[15] = new string[] { "?", "?", "?", "Umbrella", "?", "?", "?", "?" },	    //"Water Park",
+			[17] = new string[] { "?", "?", "Shovel", "?", "?", "?", "?", "?" },		//"Circus",
+			[20] = new string[] { "?", "Rake", "?", "?", "?", "?", "?", "?" },			//"Dino",
+			[21] = new string[] { "Garbage Can", "?", "?", "?", "?", "?", "?", "?" },	//"Medieval",
+			[22] = new string[] { "?", "?", "?", "?", "?", "?", "?", "Wrench" },		//"Undersea",
+		};
+
+		public static Dictionary<string, bool> getLevelTraps()
+		{
+			Dictionary<string, bool> levelTraps = new Dictionary<string, bool>();
+
+			uint lvl = Memory.getLevel();
+			uint? lvlFlagAddr = levelTrapMap[lvl];
+
+			if (lvlFlagAddr.HasValue)
+			{
+				byte[] buff = new byte[1];
+				DolphinAccessor.readFromRAM((uint)lvlFlagAddr, ref buff, 1, false);
+
+				BitArray trapFlags = new BitArray(buff);
+				Helpers.reverseBits(trapFlags);
+
+				for (int i = 0; i <= 7; i++)
+				{
+					if (trapBitMap[lvl][i] != "?")
+					{
+						levelTraps.Add(trapBitMap[lvl][i], trapFlags[i]);
+					}
+				}
+			}
+
+			return levelTraps;
+
+		}
+
+	}
 }
--- ScoobyNET/Unmasked/Memory.cs
+++ ScoobyNET/Unmasked/Memory.cs
@@ -30,11 +30,26 @@ namespace ScoobyNET.Unmasked
             return buff[0];
         }
 
-       public static uint getFoodID()
+        public static float[] getPosition()
         {
             byte[] buff = new byte[4];
-            DolphinAccessor.readFromRAM(0x55999C, ref buff, 4, true);
-            return BitConverter.ToUInt32(buff, 0);
+            DolphinAccessor.readFromRAM(0x558854, ref buff, 4, true);
+            uint baseaddr = BitConverter.ToUInt32(buff, 0);
+
+            if(baseaddr == 0)
+                return new float[3];
+
+            baseaddr -= 0x80000000;
+            buff = new byte[12];
+
+            DolphinAccessor.readFromRAM(baseaddr + 0x30, ref buff, 12, true);
+
+            float[] coordinates = new float[3];
+            coordinates[0] = BitConverter.ToSingle(buff, 8);
+            coordinates[1] = BitConverter.ToSingle(buff, 4);
+            coordinates[2] = BitConverter.ToSingle(buff, 0);
+
+            return coordinates;
         }
 
     }
